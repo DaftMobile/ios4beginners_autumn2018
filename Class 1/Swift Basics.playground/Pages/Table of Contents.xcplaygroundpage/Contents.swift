b480e170fbf0ff4b10b59d78f1185fa18c05ed6b
Michal Dabrowski <michal.dabrowski@daftcode.pl>
2018-11-12 15:31:37 +0100

    Add class 1 materials

---
+++ Class 1/Swift Basics.playground/Pages/Table of Contents.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+/*:
+# Podstawy iOS MiMUW
+
+## Spis treści
+
+1.	[Stałe i zmienne](Constants%20and%20Variables)
+2.	[Stringi](Strings)
+3.	[Kolekcje](Collections)
+4.	[Optionale](Optionals)
+5.	[Control Flow](Control%20Flow)
+6.	[Funkcje](Functions)
+7.	[Klasy](Classes)
+*/
