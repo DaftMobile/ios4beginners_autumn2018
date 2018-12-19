04cd5a49dba81eb935593826d01907a06cb3f89f
Michal Dabrowski <michal.dabrowski@daftcode.pl>
2018-12-19 10:53:44 +0100

    Add class 6 playground

---
+++ Class 6/Class6.playground/Pages/NavigationController.xcplaygroundpage/Contents.swift
@@ -0,0 +1,51 @@
+//: [Previous](@previous)
+
+import UIKit
+import PlaygroundSupport
+
+class ParentViewController: UIViewController {
+
+	override func loadView() {
+		view = UIView()
+		view.backgroundColor = .white
+		let pushButton = UIButton(type: .system)
+		pushButton.addTarget(self, action: #selector(push), for: .touchUpInside)
+		pushButton.setTitle("Push!", for: [])
+		view.addSubview(pushButton)
+
+		view.addConstraint(NSLayoutConstraint.init(item: pushButton, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0))
+
+		view.addConstraint(NSLayoutConstraint.init(item: pushButton, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1, constant: 0))
+		pushButton.translatesAutoresizingMaskIntoConstraints = false
+	}
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		title = "Parent"
+	}
+
+	@objc func push() {
+		//Step 1: Create child view controller
+		//Step 2: Push child onto navigation stack
+		let childViewController = ChildViewController()
+		self.navigationController?.pushViewController(childViewController, animated: true)
+
+	}
+}
+
+class ChildViewController: UIViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		title = "Child"
+	}
+
+	override func loadView() {
+		view = UIView()
+		view.backgroundColor = .yellow
+	}
+}
+
+let navigationController = UINavigationController(rootViewController: ParentViewController())
+
+PlaygroundPage.current.liveView = navigationController
+//: [Next](@next)
